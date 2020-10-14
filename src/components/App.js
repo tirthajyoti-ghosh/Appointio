ba3f338b19262e34324568c34c8a2a1ec8d5da9c
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-10-14 20:00:03 +0530

    Create AppRoutes for handling routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-import Appointments from '../containers/Appointments';
-import Apartments from '../containers/Apartments';
-import ApartmentDetails from '../containers/ApartmentDetails';
-import ApartmentsByType from '../containers/ApartmentsByType';
-import Login from '../containers/auth/Login';
-import Registrations from '../containers/auth/Registrations';
-import SideNav from '../containers/Sidenav';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './AppRoutes';
 
 const App = () => (
   <Router>
-    <main>
-      <SideNav />
-      <Route exact path="/" component={Apartments} />
-      <Route exact path="/appointments" component={Appointments} />
-      <Route exact path="/types/:typeId/:typeName" component={ApartmentsByType} />
-      <Route exact path="/apartments/:apartmentId" component={ApartmentDetails} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Registrations} />
-    </main>
+    <AppRoutes />
   </Router>
 );
 
