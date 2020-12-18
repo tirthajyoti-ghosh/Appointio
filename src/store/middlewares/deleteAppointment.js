75eb65b6ca54e61a80862f3f3210916c9badb40d
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-12-18 15:43:03 +0530

    Update redux middleware to accept token parameter

--- src/store/middlewares/deleteAppointment.js
+++ src/store/middlewares/deleteAppointment.js
@@ -1,8 +1,8 @@
 import deleteAppointment from '../../API/deleteAppointment';
 import { DELETE_APPOINTMENTS } from '../../constants';
 
-const deleteAppointmentAction = (appointmentId, appointments) => dispatch => {
-  deleteAppointment(appointmentId)
+const deleteAppointmentAction = (appointmentId, appointments, token) => dispatch => {
+  deleteAppointment(appointmentId, token)
     .then(status => {
       if (status === 200) {
         dispatch({ type: DELETE_APPOINTMENTS, appointmentId, appointments });
